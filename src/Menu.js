05e4d1d3aa13a8ad89432ed2b5acd1df18d46feb
helllth <th@cismet.de>
2021-07-20 15:37:26 +0200

    added help section for filtering

--- src/Menu.js
+++ src/Menu.js
@@ -113,6 +113,43 @@ const MyMenu = () => {
       shownFeatures?.length || "0"
     } in der Karte)`;
   };
+  const configurableDocBlocks = getSimpleHelpForTM(topicMapTitle, simpleHelp);
+  configurableDocBlocks[0].configs.splice(5, 0, {
+    title: "Filtern",
+    bsStyle: "warning",
+    contentBlockConf: {
+      type: "REACTCOMP",
+      content: (
+        <div>
+          <p>
+            Im Bereich &quot;<strong>Filter</strong>&quot; können Sie im Anwendungsmenü{" "}
+            <Icon name='bars' /> die in der Karte angezeigten Klimaorte so ausdünnen, dass nur die
+            für Sie interessanten Orte übrig bleiben. Dabei umfasst die Filterung die Angebote an
+            den Klimastandorten, wobei sich ein Angebot aus einem Thema und einer Kategorie ergibt.
+            Standardmäßig sind die Einstellungen hier so gesetzt, dass alle verfügbaren Klimaorte
+            angezeigt werden.
+          </p>
+          <p>
+            Ihnen stehen somit zwei Filterkriterien zur Verfügung: "Themen" und "Kategorien".
+            Innerhalb dieser Kriterien können sie in einer alphabetisch sortieren Menge an
+            Schlagworten (Tags) bestimmte Begriffe per Mausklick selektieren bzw. deselektieren; die
+            Auswahl aller bzw. keines der Schlagworte erfolgt über die Schaltfläche{" "}
+            <a class='renderAsLink'>alle</a> bzw. <a class='renderAsLink'>keine</a>.
+          </p>
+          <p>
+            Ihre Einstellungen werden direkt in der blauen Titelzeile des Bereichs "
+            <strong>Filter</strong>" und in dem Donut-Diagramm, das Sie rechts neben oder unter den
+            Filteroptionen finden, ausgewertet. Die Titelzeile zeigt die Gesamtanzahl der Klimaorte,
+            die den von Ihnen gesetzten Filterbedingungen entsprechen. Das Donut-Diagramm zeigt
+            zusätzlich die Verteilung der Klimaorte entsprechend der Filterkriterien "Themen" oder
+            "Kategorien". Bewegen Sie dazu den Mauszeiger auf eines der farbigen Segmente des
+            Diagramms. Die Farben des Donut-Diagramms entsprechen den farbigen Hintergründen der
+            Schlagworte aus dem Filterkriterium "Themen".
+          </p>
+        </div>
+      ),
+    },
+  });
 
   return (
     <CustomizationContextProvider
@@ -240,9 +277,7 @@ const MyMenu = () => {
             sectionKey='help'
             sectionTitle='Kompaktanleitung'
             sectionBsStyle='default'
-            sectionContent={
-              <ConfigurableDocBlocks configs={getSimpleHelpForTM(topicMapTitle, simpleHelp)} />
-            }
+            sectionContent={<ConfigurableDocBlocks configs={configurableDocBlocks} />}
           />,
         ]}
       />
