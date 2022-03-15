c0325362590d992fcc234194c39e6f6c03f4094d
helllth <th@cismet.de>
2022-03-15 17:36:37 +0100

    added linkProduction and show urlParam

--- src/KlimaorteMap.js
+++ src/KlimaorteMap.js
@@ -7,9 +7,11 @@ import { md5FetchText } from "react-cismap/tools/fetching";
 import { getGazDataForTopicIds } from "react-cismap/tools/gazetteerHelper";
 import "react-cismap/topicMaps.css";
 import GenericInfoBoxFromFeature from "react-cismap/topicmaps/GenericInfoBoxFromFeature";
+import InfoBoxFotoPreview from "react-cismap/topicmaps/InfoBoxFotoPreview";
+
 import TopicMapComponent from "react-cismap/topicmaps/TopicMapComponent";
 import "./App.css";
-import MyMenu from "./Menu";
+import MyMenu, { getFilterInfo } from "./Menu";
 import InfoPanel from "./SecondaryInfo";
 
 import { dataHost } from "./App";
@@ -17,7 +19,12 @@ import {
   FeatureCollectionContext,
   FeatureCollectionDispatchContext,
 } from "react-cismap/contexts/FeatureCollectionContextProvider";
-import { TopicMapDispatchContext } from "react-cismap/contexts/TopicMapContextProvider";
+import {
+  TopicMapContext,
+  TopicMapDispatchContext,
+} from "react-cismap/contexts/TopicMapContextProvider";
+import { removeQueryPart } from "react-cismap/tools/routingHelper";
+import { LightBoxDispatchContext } from "react-cismap/contexts/LightBoxContextProvider";
 const getGazData = async (setGazData) => {
   const prefix = "GazDataForStories";
   const sources = {};
@@ -41,14 +48,65 @@ function KlimaorteMap() {
   const { setSelectedFeatureByPredicate, setFilterState } = useContext(
     FeatureCollectionDispatchContext
   );
-  const { selectedFeature, items } = useContext(FeatureCollectionContext);
+  const lightBoxDispatchContext = useContext(LightBoxDispatchContext);
+  const { selectedFeature, items, shownFeatures, filterState } =
+    useContext(FeatureCollectionContext);
   const { zoomToFeature } = useContext(TopicMapDispatchContext);
+  const { history } = useContext(TopicMapContext);
 
   const [gazData, setGazData] = useState([]);
   useEffect(() => {
     getGazData(setGazData);
   }, []);
-  //   console.log("selectedFeature", selectedFeature);
+
+  useEffect(() => {
+    const handleShow = (search) => {
+      //check whether shownFeatures is undefined
+      //then the map is not ready wait for it
+      if (shownFeatures === undefined || items === undefined) {
+        return;
+      }
+      const show = new URLSearchParams(search).get("show");
+      const foundShow = show != null;
+      if (foundShow === true) {
+        //check whether the feature is already shown
+        const foundFeature = shownFeatures.find((f) => f.properties.standort.id === parseInt(show));
+        if (foundFeature !== undefined) {
+          setSelectedFeatureByPredicate((feature) => {
+            try {
+              return parseInt(feature.properties.standort.id) === parseInt(show);
+            } catch (e) {
+              return false;
+            }
+          });
+          history.push(removeQueryPart(search, "show"));
+        } else {
+          //check whether the feature is in the items list
+          const foundFeature = items.find((i) => i.standort.id === parseInt(show));
+
+          if (foundFeature !== undefined) {
+            const { themen } = getFilterInfo(items);
+            setFilterState({ ...filterState, themen });
+            // reset the filter
+          } else {
+            console.log("Objekt mit Standort.ID=" + show + " nicht gefunden");
+            history.push(removeQueryPart(search, "show"));
+          }
+        }
+
+        // setFilterState({ kampagnen: [] });
+        // setAppMenuVisible(true);
+        // setTimeout(() => {
+        //   setAppMenuActiveMenuSection("filter");
+        // }, 50);
+      }
+    };
+
+    handleShow(history.location.search);
+    return history.listen(() => {
+      handleShow(history.location.search);
+    });
+  }, [history, shownFeatures, items]);
 
   let weitereAngebote;
   const angebot = selectedFeature?.properties;
@@ -61,6 +119,28 @@ function KlimaorteMap() {
       weitereAngebote.length > 0 ||
       selectedFeature?.properties?.bemerkung !== undefined ||
       selectedFeature?.properties?.kommentar !== undefined;
+  }
+
+  const linkProduction = new URLSearchParams(history.location.search).get("linkProduction");
+  const linkProductionEnabled = linkProduction != null;
+
+  let secondaryInfoBoxElements = [
+    <InfoBoxFotoPreview
+      lightBoxDispatchContext={lightBoxDispatchContext}
+      currentFeature={selectedFeature}
+    />,
+  ];
+
+  if (linkProductionEnabled) {
+    secondaryInfoBoxElements.push(
+      <a
+        href={window.location.href + "&show=" + selectedFeature?.properties?.standort?.id}
+        target='_blank'
+        rel='noreferrer'
+      >
+        π
+      </a>
+    );
   }
 
   return (
@@ -85,6 +165,7 @@ function KlimaorteMap() {
             noCurrentFeatureTitle: "Keine Klimaorte gefunden",
             noCurrentFeatureContent: "",
           }}
+          secondaryInfoBoxElements={secondaryInfoBoxElements}
         />
       }
       secondaryInfo={<InfoPanel />}
