697fe5320766a1b1d57259a56467eb2f69a25bef
helllth <th@cismet.de>
2022-04-06 16:11:18 +0200

    imporve dictionary

--- src/helper/createItemsDictionary.js
+++ src/helper/createItemsDictionary.js
@@ -1,12 +1,19 @@
 const createItemsDictionary = (items) => {
-  const standorteInRouten = [];
+  const allStandorteInRouten = [];
+  const standorteInRouten = {};
+  const angeboteInRouten = {};
   const angeboteInStandorte = {};
   const routen = {};
   const angebote = {};
 
+  console.log("xxx createItemsDictionary");
+
   //combined run
   // save all standortids in angeboteInStandorte
   // save all ort.id's that are in routen
+
+  let additionalSelectableItemsCount = 0;
+
   for (const item of items) {
     if (item.typ === "route") {
       //
@@ -14,10 +21,25 @@ const createItemsDictionary = (items) => {
       //orte in routen
       for (const ort of item.routenpunkte) {
         if (ort.typ === "klimaort") {
-          standorteInRouten.push(ort.id);
+          allStandorteInRouten.push(ort.id);
+          const currentStandortInCache = standorteInRouten[item.id];
+          if (currentStandortInCache) {
+            currentStandortInCache.push(ort.id);
+          } else {
+            standorteInRouten[item.id] = [ort.id];
+          }
         } else if (ort.typ === "aussichtspunkt") {
+          additionalSelectableItemsCount++;
+
+          ort.routen = [{ id: item.id, typ: "route", name: item.name }]; // achtung hier noch array möglichkeit vorsehen
           for (const aussichtspunktort of ort.klimaorte) {
-            standorteInRouten.push(aussichtspunktort.id);
+            allStandorteInRouten.push(aussichtspunktort.id);
+            const currentStandortInCache = standorteInRouten[item.id];
+            if (currentStandortInCache) {
+              currentStandortInCache.push(aussichtspunktort.id);
+            } else {
+              standorteInRouten[item.id] = [aussichtspunktort.id];
+            }
           }
         }
       }
@@ -32,7 +54,23 @@ const createItemsDictionary = (items) => {
     }
   }
 
-  const dict = { items, standorteInRouten, angeboteInStandorte, angebote, routen };
+  for (const route of Object.values(routen)) {
+    for (const standortId of standorteInRouten[route.id]) {
+      for (const angebotId of angeboteInStandorte[standortId]) {
+        angebote[angebotId].routen = [{ id: route.id, typ: "route", name: route.name }]; // achtung hier noch array möglichkeit vorsehen
+      }
+    }
+  }
+
+  const dict = {
+    items,
+    allStandorteInRouten,
+    standorteInRouten,
+    angeboteInStandorte,
+    angebote,
+    routen,
+    additionalSelectableItemsCount,
+  };
   console.log("xxx dictionary", dict);
 
   return dict;
