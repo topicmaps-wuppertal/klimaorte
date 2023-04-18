c1fd330c00e37f3fdf5ae2c3c5240243133ffeb7
Thorsten Hell <thorsten.hell@cismet.de>
2023-04-18 09:58:43 +0200

    refactor to sep verlauf

---
+++ src/secondaryInfo/Verlauf.js
@@ -0,0 +1,81 @@
+import React, { useContext } from "react";
+import { FeatureCollectionContext } from "react-cismap/contexts/FeatureCollectionContextProvider";
+
+import { Timeline } from "antd";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay, faFlagCheckered } from "@fortawesome/free-solid-svg-icons";
+import { getSymbolSVGGetter } from "react-cismap/tools/uiHelper";
+
+export default function Verlauf() {
+  const { selectedFeature, items, allFeatures, itemsDictionary } = useContext(
+    FeatureCollectionContext
+  );
+  const item = selectedFeature?.properties;
+
+  const routenverlauf4Timeline = [
+    {
+      dot: <FontAwesomeIcon icon={faPlay} />,
+      // label: "0 m",
+      children: "Startpunkt",
+    },
+  ];
+
+  for (const routenpunkt of item.routenpunkte || []) {
+    let dot = [];
+    if (routenpunkt.typ === "klimaort") {
+      const angebote = itemsDictionary.angeboteInStandorte[routenpunkt.id];
+      for (const angebotId of angebote || []) {
+        const feature4Punkt = allFeatures.find(
+          (f) => f.properties.id === angebotId && f.featuretype === "ort"
+        );
+
+        dot.push(
+          getSymbolSVGGetter(
+            feature4Punkt?.properties?.svgBadge,
+            feature4Punkt?.properties?.svgBadgeDimension
+          )(25, feature4Punkt?.properties.color, "angebot_" + angebotId)
+        );
+      }
+    } else {
+      //handelt zwischenstopps &  pois
+      const feature4Punkt = allFeatures.find(
+        (f) =>
+          f.properties.id === routenpunkt.id &&
+          f.featuretype === routenpunkt.typ
+      );
+
+      dot = [
+        getSymbolSVGGetter(
+          feature4Punkt?.properties?.svgBadge,
+          feature4Punkt?.properties?.svgBadgeDimension
+        )(
+          25,
+          feature4Punkt?.properties.color,
+          "badgefor_" + routenpunkt.typ + "_" + routenpunkt.id
+        ),
+      ];
+    }
+
+    // console.log("yy routenpunkt", routenpunkt, feature4Punkt);
+
+    routenverlauf4Timeline.push({
+      label: (
+        <span style={{ paddingRight: dot.length * 13 }}>
+          {Math.round(routenpunkt.station / 100) * 100 + " m"}
+        </span>
+      ),
+      children: (
+        <span style={{ paddingLeft: dot.length * 13 }}>{routenpunkt.name}</span>
+      ),
+      dot,
+    });
+  }
+
+  routenverlauf4Timeline.push({
+    //label: "x m",
+    dot: <FontAwesomeIcon icon={faFlagCheckered} />,
+    children: "Zielpunkt",
+  });
+  return <Timeline mode="left" items={routenverlauf4Timeline} />;
+}
